338c1f4fd6993add5dffcf35f0e94e1c69adccab
fongyuqihang <369285160@qq.com>
2023-03-19 16:54:20 +0800

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.amber,
       ),
       home: const MyHomePage(title: '桃桃有趣'),
     );
